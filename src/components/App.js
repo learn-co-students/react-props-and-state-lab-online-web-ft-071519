275ac5c4527a94c506974b7f3e34ab1bbbc8e456
nicaa0695 <nicaa0695@gmail.com>
2019-11-20 15:43:58 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,29 @@ class App extends React.Component {
       }
     }
   }
+  onAdoptPet = petId => {
+    this.state.pets.forEach(myFunction);
+
+
+    function myFunction(item) {
+      if (item.id = petId) {
+        item.isAdopted = true
+      }
+    }
+    return this.state
+  }
+  fetchPet = () => {
+
+    if (this.state.filters.type === "cat" || this.state.filters.type === "dog" || this.state.filters.type === "micropig") {
+      fetch("/api/pets?type=" + this.state.filters.type)
+        .then(res => res.json())
+        .then(pets => this.setState({ pets: pets }));
+    } else {
+      fetch("/api/pets")
+        .then(res => res.json())
+        .then(pets => this.setState({ pets: pets }));
+    }
+  }
 
   render() {
     return (
@@ -24,10 +47,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+            <Filters onChangeType={this.state.filters}
+                onFindPetsClick={this.fetchPet} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+            <PetBrowser onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
