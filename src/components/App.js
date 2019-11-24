2d608ab757244892b3a08a0869aed52795242939
BaronLip <baronlip@gmail.com>
2019-11-23 19:06:32 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,37 +3,110 @@ import React from 'react'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
+// 
+
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
+    constructor() {
+        super()
+        this.state = {
+            pets: [],
+            filters: {
+                type: 'all'
+            }
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className="ui container">
-        <header>
-          <h1 className="ui dividing header">React Animal Shelter</h1>
-        </header>
-        <div className="ui container">
-          <div className="ui grid">
-            <div className="four wide column">
-              <Filters />
+    setType = (typeValue) => {
+        this.setState({
+            filters: {
+                ...this.state.filters,
+                type: typeValue
+            }
+        })
+    }
+    
+    fetchPets = () => {
+        if (this.state.filters.type === "all") {
+            fetch("/api/pets")
+              .then(response => {
+                return response.json();
+              })
+              .then(object => {
+                this.setState({
+                  pets: object
+                });
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+        } else {
+            fetch("/api/pets?type=" + this.state.filters.type)
+            .then(response => {
+                return response.json();
+            })
+            .then(object => {
+                this.setState({
+                pets: object
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        }
+    }
+
+    adoptPet = (event) => {
+        console.log(
+            event.target.id, 
+            "this is getting the petId.", 
+            this.state.pets
+        )
+        // // Map over the pets array in state,
+        // // Does it's id match the passed in petID?,
+        // // If so, change it's isAdopted value to true, 
+        // // otherwise, leave it alone.
+
+        // const pets = this.state.pets.map(p => {
+        //     return p.id === petId ? { ...p, isAdopted: true } : p;
+        // });
+        
+        // // Afterwards, replace our current state's pets array with the update pets array.
+        // this.setState({ pets: pets });
+
+        const petId = event.target.id
+        const updatedPets = this.state.pets.map((pet) => {
+            return pet.id === petId ? { ...pet, isAdopted: true } : pet;
+        });
+
+        this.setState({
+            pets: updatedPets
+        });
+
+    }
+    
+    
+    render() {
+        return (
+            <div className="ui container">
+                <header>
+                    <h1 className="ui dividing header">React Animal Shelter</h1>
+                </header>
+                <div className="ui container">
+                    <div className="ui grid">
+                        <div className="four wide column">
+                            <Filters 
+                                onChangeType={this.setType}
+                                onFindPetsClick={this.fetchPets}
+                            />
+                        </div>
+                        <div className="twelve wide column">
+                            <PetBrowser pets={this.state.pets} onAdoptPet={this.adoptPet}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="twelve wide column">
-              <PetBrowser />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default App
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
-  }
+
+    render() {
+        // By mapping it to the Pet class, petCards becomes an array of HTML elements. Which can then be returned. 
+        let petsCards = this.props.pets.map(pet =>
+            <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet}/>    
+        )
+        return (
+            <div className="ui cards">
+                {petsCards}
+            </div>
+        )
+    }
 }
 
 export default PetBrowser
